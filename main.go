38d97144d2f332c794b3b7a72fc88c04370643d8
Dragan Glumac <dragan.glumac@10xbanking.com>
2020-09-06 08:10:24 +0100

    Adding a REPL and the driver in main package.

---
+++ main.go
@@ -0,0 +1,21 @@
+// main.go
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is Monkey programming language!\n", user.Username)
+	fmt.Printf("Type .exit or .quit to end the REPL session.\n")
+	fmt.Printf("Feel free to type in commands.\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ repl/repl.go
@@ -0,0 +1,37 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		if line == ".quit" || line == ".exit" {
+			fmt.Printf("Bye!\n")
+			return
+		}
+
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
