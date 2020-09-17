e004cfddaeacea14ef3d79f319e47c4a0fe3e37c
Dragan Glumac <dragan.glumac@10xbanking.com>
2020-09-17 09:22:30 +0100

    We can now evaluate integer infix expressions.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -32,6 +32,11 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 
 	return nil
@@ -85,4 +90,43 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object,) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object,) object.Object {
+	leftVal := 	left.(*object.Integer).Value
+	rigthVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rigthVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rigthVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rigthVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rigthVal}
+	case "<":
+		return nativeBooToBooleanObject(leftVal < rigthVal)
+	case ">":
+		return nativeBooToBooleanObject(leftVal > rigthVal)
+	case "==":
+		return nativeBooToBooleanObject(leftVal == rigthVal)
+	case "!=":
+		return nativeBooToBooleanObject(leftVal != rigthVal)
+	default:
+		return NULL
+	}
 }
